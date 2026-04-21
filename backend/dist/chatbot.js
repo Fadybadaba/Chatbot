2ef2e5d200441261763c44cd48b3761c324ff794
Fadybadaba <fadi.john3@gmail.com>
2026-04-22 00:21:47 +0200

    fixed for vercel

--- backend/dist/chatbot.js
+++ backend/dist/chatbot.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,7 +39,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createChatRouter = createChatRouter;
 const express_1 = require("express");
 const multer_1 = __importDefault(require("multer"));
-const pdf_parse_1 = require("pdf-parse");
 const email_1 = require("./email");
 const approvedCvsStore_1 = require("./approvedCvsStore");
 const aiChat_1 = require("./aiChat");
@@ -106,7 +138,8 @@ function evaluateCvForJob(extractedText, jobTitle) {
     };
 }
 async function extractTextFromPdfBuffer(fileBuffer) {
-    const parser = new pdf_parse_1.PDFParse({ data: fileBuffer });
+    const { PDFParse } = await Promise.resolve().then(() => __importStar(require('pdf-parse')));
+    const parser = new PDFParse({ data: fileBuffer });
     const parsed = await parser.getText();
     return (parsed.text || '').toString();
 }
